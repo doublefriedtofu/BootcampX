1052610fe0789ba953d8702fb5a7c9b7848b7fd4
doublefriedtofu <hurdonghyuk@gmail.com>
2022-11-16 22:55:41 -0500

    updated to Compass version of the query parameters

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,18 +7,15 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-const cohortName = process.argv[2]
-const maxResult = process.argv[3]
-
 pool.query(`
-SELECT students.id, students.name AS name, cohorts.name AS cohort
+SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
-JOIN cohorts ON students.cohort_id = cohorts.id
-WHERE cohorts.name LIKE '${cohortName}%'
-LIMIT ${maxResult};
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name LIKE '%${process.argv[2]}%'
+LIMIT ${process.argv[3] || 5};
 `)
 .then(res => {
   res.rows.forEach(user => {
-    console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort} cohort`);
+    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
   })
-});
+}).catch(err => console.error('query error', err.stack));
