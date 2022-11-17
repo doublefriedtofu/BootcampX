f1bfa36913d678b2dab827b197bf3382890f88c2
doublefriedtofu <hurdonghyuk@gmail.com>
2022-11-16 22:54:47 -0500

    my version of finding query parameters using process argv"

---
+++ bootcamp_app/students.js
@@ -0,0 +1,24 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+const cohortName = process.argv[2]
+const maxResult = process.argv[3]
+
+pool.query(`
+SELECT students.id, students.name AS name, cohorts.name AS cohort
+FROM students
+JOIN cohorts ON students.cohort_id = cohorts.id
+WHERE cohorts.name LIKE '${cohortName}%'
+LIMIT ${maxResult};
+`)
+.then(res => {
+  res.rows.forEach(user => {
+    console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort} cohort`);
+  })
+});
